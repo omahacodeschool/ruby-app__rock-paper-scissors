15428eff0318a69aed86968c893c7c65e8f21dbf
Meagan Jungman <meagan.jungman@gmail.com>
2016-01-30 00:16:37 -0600

    tightened && began refactoring

--- lib/test.rb
+++ lib/test.rb
@@ -15,80 +15,89 @@ def determine_player_choice(choice_input)
   when "spock"
     return "spock"
   else
-  # eventually this needs to call back the player choice method so the player can enter a valid option
-    return "i'm a big doo doo head" 
+    puts "That's not a valid choice, you goon. Pick again."
+    determine_player_choice(choice_input)
   end
 
 end
 
-def new_game_player_choice (new_choice)
+def determine_set_winner(player1, player2, player1_games_won, player2_games_won, tied_games)
+
+  if player1_games_won > player2_games_won
+      return "#{player1.keys.join("")} won the set of games with a score of #{player1_games_won} vs. #{player2_games_won}. There were #{tied_games} tied games."
+
+  elsif player1_games_won < player2_games_won
+      return "#{player2.keys.join("")} won the set of games with a score of #{player2_games_won} vs. #{player1_games_won}. There were #{tied_games} tied games."
+      
+
+  else player1_games_won == player2_games_won
+      return "It was a tie, guys. You're both just super duper."
+  end
 
 end
+
 player1_choice_input = ""
 player2_choice_input = ""
 puts "Let's play 'Rock, Paper Scissors!'"
-print "Player, enter your name:  "
+print "How many games do you want to play?  "
+max_games = gets.chomp.to_i
+print "PLAYER ONE, enter your name:  "
 player1_name = gets.chomp
-player1_choice_input = determine_player_choice(player1_choice_input)
+print "PLAYER TWO, enter your name:  "
 player2_name = gets.chomp
-player2_choice_input = determine_player_choice(player2_choice_input)
 
 
+played_games = 0
+player1_games_won = 0
+player2_games_won = 0
+tied_games = 0
 
-$played_games = 0
-$player1_games_won = 0
-$player2_games_won = 0
-max_games = 3
-
-# key = player. value = input
 player1 = {player1_name => player1_choice_input}
 player2 = {player2_name => player2_choice_input}
-puts player1
-puts player2
 
 
-    
-    if player1.values == player2.values
-       puts " #{player1.values.join("")} vs. #{player2.values.join("")} results in a tie."
-       $played_games += 1
-    end
+while played_games < max_games do
+  played_games += 1
+  player1_choice_input = determine_player_choice(player1_choice_input)
+  player2_choice_input = determine_player_choice(player2_choice_input)
+  player1 = {player1_name => player1_choice_input}
+  player2 = {player2_name => player2_choice_input}
 
-    if player1.values.include?("rock") && player2.values.include?("paper")
-             puts " #{player1.values.join("")} vs. #{player2.values.join("")}: #{player2.keys.join("")} wins!"
-             $played_games += 1
-             $player1_games_won += 1
-    end
+  if player1.values == player2.values
+    puts " #{player1.values.join("")} vs. #{player2.values.join("")} results in a tie."
+    tied_games += 1
+  end
 
-    if player1.values.include?("rock") && player2.values.include?("scissors")
-             puts " #{player1.values.join("")} vs. #{player2.values.join("")}: #{player1.keys.join("")} wins!"
-             $played_games += 1
-             $player1_games_won += 1
-    end
+  if player1.values.include?("rock") && player2.values.include?("paper")
+    puts " #{player1.values.join("")} vs. #{player2.values.join("")}: #{player2.keys.join("")} wins!"
+    player1_games_won += 1
+   end
 
-    if player1.values.include?("scissors") && player2.values.include?("paper")
-             puts " #{player1.values.join("")} vs. #{player2.values.join("")}: #{player1.keys.join("")} wins!"
-             $played_games += 1
-             $player1_games_won += 1
-    end
+  if player1.values.include?("rock") && player2.values.include?("scissors")
+    puts " #{player1.values.join("")} vs. #{player2.values.join("")}: #{player1.keys.join("")} wins!"
+    player1_games_won += 1
+  end
 
-    if player1.values.include?("scissors") && player2.values.include?("rock")
-             puts " #{player1.values.join("")} vs. #{player2.values.join("")}: #{player2.keys.join("")} wins!"
-             $played_games += 1
-             $player2_games_won += 1
-    end
+  if player1.values.include?("scissors") && player2.values.include?("paper")
+    puts " #{player1.values.join("")} vs. #{player2.values.join("")}: #{player1.keys.join("")} wins!"
+    player1_games_won += 1
+  end
 
-    if player1.values.include?("paper") && player2.values.include?("rock")
-             puts " #{player1.values.join("")} vs. #{player2.values.join("")}: #{player1.keys.join("")} wins!"
-             $played_games += 1
-             $player1_games_won += 1
-    end
+  if player1.values.include?("scissors") && player2.values.include?("rock")
+    puts " #{player1.values.join("")} vs. #{player2.values.join("")}: #{player2.keys.join("")} wins!"
+    player2_games_won += 1
+  end
 
-    if player1.values.include?("paper") && player2.values.include?("scissors")
-             puts " #{player1.values.join("")} vs. #{player2.values.join("")}: #{player2.keys.join("")} wins!"
-             $played_games += 1
-             $player2_games_won += 1
-             player1.each{ |key,value| my_hash[key] = "" }
-             player2.each{ |key,value| my_hash[key] = "" }
-    end 
+  if player1.values.include?("paper") && player2.values.include?("rock")
+    puts " #{player1.values.join("")} vs. #{player2.values.join("")}: #{player1.keys.join("")} wins!"
+    player1_games_won += 1
+  end
 
+  if player1.values.include?("paper") && player2.values.include?("scissors")
+    puts " #{player1.values.join("")} vs. #{player2.values.join("")}: #{player2.keys.join("")} wins!"
+    player2_games_won += 1
+  end 
 
+end
+
+puts determine_set_winner(player1, player2, player1_games_won, player2_games_won, tied_games)
---
+++ lib/test2.rb
@@ -0,0 +1,18 @@
+count = 0
+
+while count < 10
+  count += 1
+  if count == 5
+    puts 'look alive 5'
+  end
+
+  if count == 3 
+    puts 'it is me 3'
+  end
+
+  if count == 7
+    puts '7 is heaven'
+  end
+
+  puts count 
+end

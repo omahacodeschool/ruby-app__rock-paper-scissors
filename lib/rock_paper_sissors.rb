cd0825fbea9bef4bc338eef2689ed7c56b9fe188
Derek Pressnall <derekpressnall@yahoo.com>
2016-01-31 17:23:58 -0600

    Function game before refactoring

--- lib/rock_paper_sissors.rb
+++ lib/rock_paper_sissors.rb
@@ -1,61 +1,70 @@
 
+def paper_rock_scissors
+  puts "\nLets play a set of paper-rock-scissors."
 
+  puts "\nGive me an odd number. Best of the 3, 5, 7... It's your choice." 
+  
+  amount_of_games_to_play = gets.chomp.to_i
 
-def paper_rock_sissors(str1, str2)
- 
-  rock_game = rock(str1, str2)
-  paper_game = paper(str1, str2)
-  sissors_game = sissors(str1, str2)
-  #game_counter = 0
-  #game_stop = 3
- 
-  #while game_counter < game_stop
-    if str1 == "sissors" 
-      sissors_game
-    elsif str1 == "paper"
-      paper_game
-    else str1 == "rock"
-      rock_game
+  player_one_wins = 0
+  player_two_wins = 0
+  games_played = 0
+
+  while games_played < amount_of_games_to_play 
+    
+    puts "\nPlayer one your turn."
+    str1 = gets.chomp.downcase
+    
+    puts "\nOkay! Player two your turn."
+    str2 = gets.chomp.downcase
+    
+    if str1 == "scissors" 
+      if str2 == "paper"
+        puts "\nPlayer one wins!"
+        player_one_wins += 1
+      elsif str2 == "scissors"
+        puts "\nIt's a tie!" 
+        games_played -= 1
+      else str2 == "rock"
+        puts "\nPlayer two wins!"
+        player_two_wins += 1
+      end
     end
-   #game_counter +=1
-  #end
+
+    if str1 == "paper"
+      if str2 == "rock"
+        puts "\nPlayer one wins!"
+        player_one_wins += 1
+      elsif str2 == "paper"
+        puts "\nIt's a tie!"
+        games_played -= 1
+      else str2 == "scissors"
+        puts "\nPlayer two wins!"
+        player_two_wins += 1
+      end 
+    end
+
+    if str1 == "rock"
+      if str2 == "scissors"
+        puts "\nPlayer one wins!"
+        player_one_wins += 1
+      elsif str2 == "rock"
+        puts "\nIt's a tie!"
+        games_played -= 1
+      else str2 == "paper"
+        puts "\nPlayer two wins!"
+        player_two_wins += 1
+      end 
+    end
+    games_played += 1
+  end
+
+  if player_one_wins > player_two_wins
+    puts "\nPlayers one is the winner!"
+  else
+    puts "\nPlayer two is the winner!"
+  end
 end
 
-def sissors(str1, str2)
- 
-  if str2 == "paper"
-    return "Player one wins!"
-  elsif str2 == "sissors"
-    return "It's a tie!"
-  else str2 == "rock"
-      return "Player two wins!"
-  end 
-end
 
-def paper(str1, str2)
- 
-  if str2 == "rock"
-    return "Player one wins!"
-  elsif str2 == "paper"
-    return "It's a tie!"
-  else str2 == "sissors"
-      return "Player two wins!"
-  end 
-end
-
-def rock(str1, str2)
- 
-  if str2 == "sissors"
-    return "Player one wins!"
-  elsif str2 == "rock"
-    return "It's a tie!"
-  else str2 == "paper"
-      return "Player two wins!"
-  end 
-end
-
-
-
-puts paper_rock_sissors("paper", "paper")
-
-
+puts paper_rock_scissors

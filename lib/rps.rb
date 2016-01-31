b8dd99858f1ae023f6193e43b46e5b62bf5a4816
Beth Haubert <bethany.haubert@gmail.com>
2016-01-31 16:53:44 -0600

    added moar tests

--- lib/rps.rb
+++ lib/rps.rb
@@ -36,41 +36,64 @@ def get_player_input
 	return gets.chomp.downcase
 end
 
-
-puts "Player one, please type your name."
-player1 = get_player_name
-
-puts "Player two, please type your name."
-player2 = get_player_name
-
-score = {"#{player1}" => 0, "#{player2}" => 0}
-
-while score.values.max < 3
-
-	puts "#{player1}" +", please type rock, paper, or scissors"
-	p1 = get_player_input
-
-	puts "#{player2}" +", please type rock, paper, or scissors"
-	p2 = get_player_input
-
-	puts "#{player1}" + " chose " + "#{p1}" + ". " + "#{player2}" + " chose " + "#{p2}" + "."
-
-	if draw?(p1, p2)
- 	 	puts "It's a draw!"
-
-	elsif if_player_1_wins?(p1, p2)
-		score["#{player1}"] += 1
-	 	puts "#{player1.capitalize}" + " wins this round."
-
-	elsif !if_player_1_wins?(p1, p2)
-		score["#{player2}"] += 1
-	  	puts "#{player2.capitalize}" + " wins this round."
-
-	end
-	puts score
+#Check to see if it's an overall draw
+#
+#score - The Hash that's storing the player's name and number of rounds won
+# p1 - The String that is player one's input
+# p2 - The String that is player two's input
+#
+#Returns true or false
+def overall_draw?(score, player1, player2)
+	score["#{player1}"] == score["#{player2}"]
 end
-puts "#{score.max_by{|k,v| v}[0].upcase}" + " WINS OVERALL!!!"
 
 
+# def game_on
+# 	puts "Player one, please type your name."
+# 	player1 = get_player_name
+
+# 	puts "Player two, please type your name."
+# 	player2 = get_player_name
+
+# 	puts "Please type how many rounds you would like to play."
+# 	number_of_games = gets.chomp.to_i
+
+# 	round_number = 0
+
+# 	score = {"#{player1}" => 0, "#{player2}" => 0}
+
+# 	while round_number < number_of_games #score.values.max < 3
+
+# 		puts "#{player1}" +", please type rock, paper, or scissors"
+# 		p1 = get_player_input
+
+# 		puts "#{player2}" +", please type rock, paper, or scissors"
+# 		p2 = get_player_input
+
+# 		puts "#{player1}" + " chose " + "#{p1}" + ". " + "#{player2}" + " chose " + "#{p2}" + "."
+
+# 		if draw?(p1, p2)
+# 	 	 	puts "It's a draw!"
+
+# 		elsif if_player_1_wins?(p1, p2)
+# 			score["#{player1}"] += 1
+# 		 	puts "#{player1.capitalize}" + " wins this round."
+
+# 		elsif !if_player_1_wins?(p1, p2)
+# 			score["#{player2}"] += 1
+# 		  	puts "#{player2.capitalize}" + " wins this round."
+
+# 		end
+# 		puts score
+# 		round_number += 1
+# 	end
+# 	if overall_draw?(score, player1, player2)
+# 		puts "No one wins overall game."
+
+# 	else !overall_draw?(score, player1, player2)
+# 		puts "#{score.max_by{|k,v| v}[0].upcase}" + " WINS OVERALL!!!"
+# 	end
+# end
 
 
+# game_on
--- tests/rps_test.rb
+++ tests/rps_test.rb
@@ -14,15 +14,29 @@ class RockPaperScissorsTest < Minitest::Test
 	  	assert_equal(false, if_player_1_wins?(p1, p2))
 	end
 
-	def test_draw
+	def test_game_draw_true
 		p1 = "rock"
 		p2 = "rock"
 		assert_equal(true, draw?(p1, p2))
 	end
 
-	def test_draw
+	def test_game_draw_false
 		p1 = "rock"
 		p2 = "scissors"
 		assert_equal(false, draw?(p1, p2))
+	end
+
+	def test_overall_draw_true
+		player1 = "Beth"
+		player2 = "Sumeet"
+		score = {"#{player1}" => 3, "#{player2}" => 3}
+		assert_equal(true, overall_draw?(score, player1, player2))
+	end
+
+	def test_overall_draw_false
+		player1 = "Beth"
+		player2 = "Sumeet"
+		score = {"#{player1}" => 3, "#{player2}" => 2}
+		assert_equal(false, overall_draw?(score, player1, player2))
 	end
 end

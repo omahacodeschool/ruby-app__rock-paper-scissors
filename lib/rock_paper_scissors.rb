0460b38c42deec48718947281002b983cbace7cd
Patrick Clark <patclark1958@gmail.com>
2016-01-30 22:12:37 -0600

    better indentation

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -1,131 +1,143 @@
 def rock_paper_scissors
 
-  puts "Welcome to Rock-Paper-Scissors!"
-  puts "The 216th most addictive game on the internet."
-  puts 
-  puts "What name do you want to use here, Player 1?"
-  puts "Enter name here:      "
-        player_1_name = gets.chomp
-  puts
-  puts  "Welcome #{player_1_name}"
-  puts  "\n\n"
-  puts "What name do you want to use here, Player 2?"
-  puts "Enter name here: "
-        player_2_name = gets.chomp
-  puts  "Welcome #{player_2_name}"     
-  puts "\n\n"
-  puts
-  puts "Determine a champion in a best of 1, 3, 5 or 7 game match."
-  puts "Choose the number of games in your match."
-       "Enter 1, 3, 5 or 7 here"
-        match_duration = gets.chomp
-        md = match_duration.to_i
-        $player_victory = (md + 1) / 2
-  puts "You will compete in a #{match_duration} game match, so"
-  puts "the first player with #{$player_victory} victories"
-  puts "is the champion."
-  puts "\n\n"
-  puts
-  puts  "I grow weary of the formalities. Play on!"
-  puts  "_______________________________________________________"
+  #INITIALIZE VARIABLE FOR PLAY AGAIN OPTION
+  y = $play_again_answer
+  while y == $play_again_answer do
+    
+    #INITIAL SETUP OF PLAYERS
+    puts "Welcome to Rock-Paper-Scissors!"
+    puts "The 216th most addictive game on the internet."
+    puts 
+    puts "What name do you want to use here, Player 1?"
+    puts  "Enter name here:      "
+          $player_1_name = gets.chomp
+    puts
+    puts  "Welcome #{$player_1_name}"
+    puts  "\n\n"
+    puts  "What name do you want to use here, Player 2?"
+    puts  "Enter name here: "
+          $player_2_name = gets.chomp
+    puts  "Welcome #{$player_2_name}"     
+    puts  "\n\n"
+    puts
+    puts  "Determine a champion in a best of 1, 3, 5 or 7 game match."
+    puts  "Choose the number of games in your match."
+          "Enter 1, 3, 5 or 7 here"
+          match_duration = gets.chomp
+          md = match_duration.to_i
+          $player_victory = (md + 1) / 2
+    puts "You will compete in a #{match_duration} game match, so"
+    puts "the first player with #{$player_victory} victories"
+    puts "is the champion."
+    puts "\n\n"
+    puts
+    puts  "I grow weary of the formalities. Play on!"
+    puts  "_______________________________________________________"
 
 
-#SCOREBOARD
+    #SCOREBOARD
 
-  $player_1_score = 0
-  $player_2_score = 0
-  puts "SCOREBOARD"
-  puts
-  puts "#{player_1_name} #{$player_1_score}"
-  puts "#{player_2_name} #{$player_2_score}"
-  puts  "_______________________________________________________"
+    $player_1_score = 0
+    $player_2_score = 0
+    puts "SCOREBOARD"
+    puts
+    puts "#{$player_1_name} #{$player_1_score}"
+    puts "#{$player_2_name} #{$player_2_score}"
+    puts  "_______________________________________________________"
   
-#GAME PLAY
+      #GAME PLAY
 
-  while $player_1_score < $player_victory && $player_2_score < $player_victory do
+    until $player_1_score == $player_victory or $player_2_score == $player_victory do
 
-#PLAY ONE GAME
+      #WEAPON OPTIONS
+      rock = "r"
+      paper = "p"
+      scissors = "s"
 
-  puts "Choose your weapon. Rock, Paper or Scissors."  
-  puts 
-  puts "#{player_1_name} type in your weapon here:       "
-        player_1_weapon = gets.chomp.downcase
-  puts
-  puts "#{player_1_name} has chosen #{player_1_weapon}"
-  puts
-  puts "#{player_2_name} type in your weapon here:       "
-        player_2_weapon = gets.chomp.downcase
-  puts     "#{player_2_name} has chosen #{player_2_weapon}"
+      #PLAY ONE GAME
+
+      puts "Choose your weapon. Rock (r), Paper (p) or Scissors (s)."  
+      puts 
+      puts "#{$player_1_name} type in your weapon here:       "
+            $player_1_weapon = gets.chomp.downcase
+      puts
+      puts "#{$player_1_name} has chosen #{$player_1_weapon}"
+      puts
+      puts "#{$player_2_name} type in your weapon here:       "
+            $player_2_weapon = gets.chomp.downcase
+      puts  "#{$player_2_name} has chosen #{$player_2_weapon}"
   
 
 
-#DETERMINE WINNER OF ONE GAME - MAIN VERSION
-  rock = "rock"
-  paper = "paper"
-  scissors = "scissors"
+      #DETERMINE WINNER OF ONE GAME - MAIN VERSION
+  
 
-  if #{player_1_weapon} == "rock" && #{player_2_weapon} == "rock" 
-    puts "It's a tie. No points awarded."
+      if $player_1_weapon == "r" && $player_2_weapon == "r" 
+        puts "It's a tie. No points awarded."
 
-  elsif #{player_1_weapon} == "rock" && #{player_2_weapon} == "scissors"
-    puts "Rock breaks scissors. Point to Player_1."
-    $player_1_score += 1
+      elsif $player_1_weapon == "r" && $player_2_weapon == "s"
+        puts "Rock breaks scissors. Point to #{$player_1_name}."
+        $player_1_score += 1
 
-  elsif #{player_1_weapon} == "rock" && #{player_2_weapon} == "paper"
-    puts "Paper covers rock. Point to Player_2."
-    $player_2_score += 1
+      elsif $player_1_weapon == "r" && $player_2_weapon == "p"
+        puts "Paper covers rock. Point to #{$player_2_name}."
+        $player_2_score += 1
 
-  elsif #{player_1_weapon} == "scissors" && #{player_2_weapon} == "rock"
-    puts "Rock breaks scissors. Point to Player_2."
-    $player_2_score += 1
+      elsif $player_1_weapon == "s" && $player_2_weapon == "r"
+        puts "Rock breaks scissors. Point to #{$player_2_name}."
+        $player_2_score += 1
 
-  elsif #{player_1_weapon} == "scissors" && #{player_2_weapon} == "scissors"
-    puts "It's a tie. No points awarded."
+      elsif $player_1_weapon == "s" && $player_2_weapon == "s"
+        puts "It's a tie. No points awarded."
 
-  elsif #{player_1_weapon} == "scissors" && #{player_2_weapon} == "paper"
-    puts "Scissors cut paper. Point to Player_1."
-    $player_1_score += 1
+      elsif $player_1_weapon == "s" && $player_2_weapon == "p"
+        puts "Scissors cut paper. Point to #{$player_1_name}."
+        $player_1_score += 1
 
-  elsif #{player_1_weapon} == "paper" && player_2_weapon == "rock"
-    puts "Paper covers rock. Point to Player_1."
-    $player_1_score += 1
+      elsif $player_1_weapon == "p" && $player_2_weapon == "r"
+        puts "Paper covers rock. Point to #{$player_1_name}."
+        $player_1_score += 1
 
-  elsif #{player_1_weapon} == "paper" && #{player_2_weapon} == "scissors"
-    puts "Scissors cut paper. Point to Player_2."
-    $player_2_score += 1
+      elsif $player_1_weapon == "p" && $player_2_weapon == "s"
+        puts "Scissors cut paper. Point to #{$player_2_name}."
+        $player_2_score += 1
 
-  elsif #{player_1_weapon} == "paper" && #{player_2_weapon} == "paper"
-    puts "It's a tie. No points awarded."
+      elsif $player_1_weapon == "p" && $player_2_weapon == "p"
+        puts "It's a tie. No points awarded."
+      end
+    end
+
+      #MATCH DISPENSATION
+
+      #ANNOUNCEMENT OF THE WINNER
+    if  $player_1_score == $player_victory 
+        puts 
+        puts "Congratulations #{$player_1_name}"
+        puts "You have won this match #{$player_1_score} games to #{$player_2_score}"
+        puts 
+
+    elsif $player_2_score == $player_victory
+        puts 
+        puts "Congratulations #{$player_2_name}"
+        puts "You have won this match #{$player_2_score} games to #{$player_1_score}"
+        puts 
+    end
+
+      #THIS IS NOT OVER, BUDDY!
+
+        puts "Would you like to play again?"
+        puts "Enter 'y' for 'yes' or 'n' for 'no"
+        $play_again_answer = gets.chomp.downcase
+        puts #{$play_again_answer}
+    if  y == #{$play_again_answer} 
+        $player_1_score = 0
+        $player_2_score = 0
+  
+    else
+        puts "Goodbye for now."
     end
   end
-
-#MATCH DISPENSATION
-
-#ANNOUNCEMENT OF THE WINNER
-  if $player_1_score == $player_victory 
-      puts "Congratulations #{$player_1_name}"
-      puts "You have won this match #{$player_1_score} games to #{$player_2_score}"
-
-  elsif $player_2_score == $player_victory
-      puts "Congratulations #{$player_2_name}"
-      puts "You have won this match #{$player_2_score} games to #{$player_1_score}"
-  end
-
-#THIS IS NOT OVER, BUDDY!
-
-  puts "Would you like to play again?"
-  puts "Enter 'y' for 'yes' or 'n' for 'no"
-      play_again_answer = (gets.chomp)
-
-  if play_again_answer == "y"
-      $player_1_score = 0
-      $player_2_score = 0
-
-  else
-  end
 end
-
-
 #-------------------------------------------------
 
 #DETERMINE WINNER OF ONE GAME - ALTERNATE VERSION
@@ -160,5 +172,8 @@ end
 
 
 #SOLITAIRE NOTE 
+
+
+
 #Generate a random answer from the computer for the solitaire version
 #rand(3)

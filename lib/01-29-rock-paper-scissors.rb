be70d37966c2dc50313f902cb3c89d654aee5d2a
Melinda Deinert <melinda.deinert@gmail.com>
2016-01-30 19:30:24 -0600

    Commentary added.

--- lib/01-29-rock-paper-scissors.rb
+++ lib/01-29-rock-paper-scissors.rb
@@ -3,11 +3,12 @@ games_needed_to_win = gets.chomp.to_i
 
 current_Score = {"Player 1"=>0, "Player 2"=>0}
 
+puts "Let's play a game of Rock-Paper-Scissors."
+
 while current_Score.values.max<games_needed_to_win
 
-  puts "Let's play a game of Rock-Paper-Scissors.
 
-  Player 1, what's your move?"
+  puts "Player 1, what's your move?"
   player_1_move = gets.chomp.downcase
 
   puts "Player 2, what's your move?"
@@ -29,16 +30,20 @@ if player_1_move != player_2_move
   end
   puts "One point for " + match_Winner + "!"
   current_Score["#{match_Winner}"] += 1
+  puts "Player 1 has #{current_Score["Player 1"]} point(s), and player 2 has #{current_Score["Player 2"]} point(s)."
 
 else player_1_move == player_2_move
-  print "That was a draw. Let's play again."
+  print "That was a draw. Let's play again. "
 
-#LOOP SHIT
 
 end
+
+if current_Score["Player 1"] >= games_needed_to_win
+  puts "Player 1 wins!"
+elsif current_Score["Player 2"] >= games_needed_to_win
+  puts "Player 2 wins!"
+else
+  next
 end
 
-player_1_move = "rock"
-player_2_move = "scissors"
-
-
+end

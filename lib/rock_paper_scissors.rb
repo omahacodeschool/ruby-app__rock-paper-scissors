a8e5b31661857d804874180c50ee5edab1c3f8ef
Kyle Sandage <ksandage08@gmail.com>
2016-01-31 17:00:30 -0600

    defined player_choice method

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -1,73 +1,62 @@
 #Rock Paper Scissors Game
-
+##Methods are Defined Here
+def player_choice
+    puts "Choose your weapon!"
+    weapon = gets.chomp.downcase
+    until weapon == "rock" || weapon == "paper" || weapon == "scissors" 
+        puts "Please chose rock paper or scissors."
+        weapon = gets.chomp.downcase
+        if weapon == "rock" || weapon == "paper" || weapon == "scissors"  
+            puts "GOOD CHOICE!"
+        end
+    end
+    return weapon
+end
+##Code Starts Here
 puts "ROCK PAPER SCISSORS!!!!"
 puts "Player 1, what is your name?"
-player1 = gets.chomp
+$player1 = gets.chomp
 puts "Player 2, what is your name?"
-player2 = gets.chomp
+$player2 = gets.chomp
 puts "How many games till victory?"
 victory_requirement = gets.chomp.to_i
-victory_count = {player1 => 0, player2 => 0}
+victory_count = {$player1 => 0, $player2 => 0}
+
+
 
 
 until victory_count.has_value?(victory_requirement)
 
+  puts "#{$player1} Choose your weapon!"
+  weapon_1 = player_choice
 
-puts "#{player1} Choose your weapon!"
-weapon_1 = gets.chomp.downcase
-
-  if weapon_1 == "rock" || weapon_1 == "paper" || weapon_1 == "scissors"  
-    puts "GOOD CHOICE!"
-  end
-
-  until weapon_1 == "rock" || weapon_1 == "paper" || weapon_1 == "scissors" 
-    puts "Please chose rock paper or scissors."
-    weapon_1 = gets.chomp.downcase
-    if weapon_1 == "rock" || weapon_1 == "paper" || weapon_1 == "scissors"  
-      puts "GOOD CHOICE!"
-    end
-  end
-
-  puts "#{player2} Choose your weapon!"
-  weapon_2 = gets.chomp.downcase
-
-  if weapon_2 == "rock" || weapon_2 == "paper" || weapon_2 == "scissors"  
-    puts "GOOD CHOICE!"
-  end
-
-  until weapon_2 == "rock" || weapon_2 == "paper" || weapon_2 == "scissors" 
-    puts "Please chose rock paper or scissors."
-    weapon_2 = gets.chomp.downcase
-    if weapon_2 == "rock" || weapon_2 == "paper" || weapon_2 == "scissors"  
-      puts "GOOD CHOICE!"
-    end
-  end
-
+  puts "#{$player2} Choose your weapon!"
+  weapon_2 = player_choice
 
   case 
   when weapon_1 == "rock" && weapon_2 == "scissors"
-    puts "#{player1} wins!"
-    victory_count[player1]+=1
+    puts "#{$player1} wins!"
+    victory_count[$player1]+=1
     puts "Current score is #{victory_count}"
   when weapon_1 == "paper" && weapon_2 == "rock"
-    puts "#{player1} wins!"
-    victory_count[player1]+=1
+    puts "#{$player1} wins!"
+    victory_count[$player1]+=1
     puts "Current score is #{victory_count}"
   when weapon_1 == "scissors" && weapon_2 == "paper"
-    puts "#{player1} wins!"
-    victory_count[player1]+=1
+    puts "#{$player1} wins!"
+    victory_count[$player1]+=1
     puts "Current score is #{victory_count}"
   when weapon_2 == "rock" && weapon_1 == "scissors"
-    puts "#{player2} wins!"
-    victory_count[player2]+=1
+    puts "#{$player2} wins!"
+    victory_count[$player2]+=1
     puts "Current score is #{victory_count}"
   when weapon_2 == "paper" && weapon_1 == "rock"
-    puts "#{player2} wins!"
-    victory_count[player2]+=1
+    puts "#{$player2} wins!"
+    victory_count[$player2]+=1
     puts "Current score is #{victory_count}"
   when weapon_2 == "scissors" && weapon_1 == "paper"
-    puts "#{player2} wins!"
-    victory_count[player2]+=1
+    puts "#{$player2} wins!"
+    victory_count[$player2]+=1
     puts "Current score is #{victory_count}"
   when weapon_2 == "rock" && weapon_1 == "rock"
     puts "Nobody wins. Tie game!"
@@ -81,6 +70,8 @@ weapon_1 = gets.chomp.downcase
   end
 end
 puts "#{victory_count.max_by{|k,v| v}[0]} is victorious!!!"
+
+
 
 
 

c6eff7f83fe0048b17c6a089d95f9a5bf1f593c4
Melinda Deinert <melinda.deinert@gmail.com>
2016-02-01 12:45:39 -0600

    find match winner and determine number of games methods finished

--- lib/01-29-rock-paper-scissors.rb
+++ lib/01-29-rock-paper-scissors.rb
@@ -1,3 +1,24 @@
+#TomDoc here
+def find_match_winner(player_1_move, player_2_move)
+  if player_1_move == "rock" && player_2_move == "scissors"
+    match_Winner = "Player 1"
+  elsif player_1_move == "rock" && player_2_move == "paper"
+    match_Winner = "Player 2"
+  elsif player_1_move == "paper" && player_2_move == "rock"
+    match_Winner = "Player 1"
+  elsif player_1_move == "paper" && player_2_move == "scissors"
+    match_Winner = "Player 2"
+  elsif player_1_move == "scissors" && player_2_move == "paper"
+    match_Winner = "Player 1"
+  elsif player_1_move == "scissors" && player_2_move == "rock"
+    match_Winner = "Player 2"
+  end
+  return match_Winner
+end
+
+
+
+
 puts "How many games should a player win in order to win the set?"
 games_needed_to_win = gets.chomp.to_i
 
@@ -16,7 +37,8 @@ while current_Score.values.max<games_needed_to_win
   player_2_move = gets.chomp.downcase
   
   if player_1_move != player_2_move
-    match_Winner = find_match_winner(player_1_move, player_2_move) #undefined method error
+
+    match_Winner = find_match_winner(player_1_move, player_2_move)
 
     puts "One point for " + match_Winner + "!"
     current_Score["#{match_Winner}"] += 1
@@ -30,32 +52,12 @@ while current_Score.values.max<games_needed_to_win
 
 if current_Score["Player 1"] >= games_needed_to_win
   puts "Player 1 wins!"
-  set_Winner = "Player 1"
 elsif current_Score["Player 2"] >= games_needed_to_win
   puts "Player 2 wins!"
-  set_Winner = "Player 2"
 else
   next
 end
 
   end
-  return set_Winner
 
 
-#TomDoc here
-def find_match_winner(player_1_move, player_2_move)
-  if player_1_move == "rock" && player_2_move == "scissors"
-    match_Winner = "Player 1"
-  elsif player_1_move == "rock" && player_2_move == "paper"
-    match_Winner = "Player 2"
-  elsif player_1_move == "paper" && player_2_move == "rock"
-    match_Winner = "Player 1"
-  elsif player_1_move == "paper" && player_2_move == "scissors"
-    match_Winner = "Player 2"
-  elsif player_1_move == "scissors" && player_2_move == "paper"
-    match_Winner = "Player 1"
-  elsif player_1_move == "scissors" && player_2_move == "rock"
-    match_Winner = "Player 2"
-  end
-  return match_Winner
-end

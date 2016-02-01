c56140113f1eb9491a486e1554ffac76e09ddae7
Melinda Deinert <melinda.deinert@gmail.com>
2016-02-01 13:05:18 -0600

    get players' moves method

--- lib/01-29-rock-paper-scissors.rb
+++ lib/01-29-rock-paper-scissors.rb
@@ -16,25 +16,38 @@ def find_match_winner(player_1_move, player_2_move)
   return match_Winner
 end
 
+#TomDoc here
+def determine_number_of_games
+  puts "How many games should a player win in order to win the set?"
+  $games_needed_to_win = gets.chomp.to_i
+  return $games_needed_to_win
+end
 
+#TomDoc here
+def get_first_players_move(player_1_move)
+  puts "Player 1, what's your move?"
+  player_1_move = gets.chomp.downcase
+  return player_1_move
+end
 
-
-puts "How many games should a player win in order to win the set?"
-games_needed_to_win = gets.chomp.to_i
+#TomDoc here
+def get_second_players_move(player_2_move)
+  puts "Player 2, what's your move?"
+  player_2_move = gets.chomp.downcase
+  return player_2_move
+end
 
 current_Score = {"Player 1"=>0, "Player 2"=>0}
 
 puts "Let's play a game of Rock-Paper-Scissors."
 
+determine_number_of_games
 
+while current_Score.values.max<$games_needed_to_win
 
-while current_Score.values.max<games_needed_to_win
+  player_1_move = get_first_players_move(player_1_move)
 
-  puts "Player 1, what's your move?"
-  player_1_move = gets.chomp.downcase
-
-  puts "Player 2, what's your move?"
-  player_2_move = gets.chomp.downcase
+  player_2_move = get_second_players_move(player_2_move)
   
   if player_1_move != player_2_move
 
@@ -50,9 +63,9 @@ while current_Score.values.max<games_needed_to_win
 
   end
 
-if current_Score["Player 1"] >= games_needed_to_win
+if current_Score["Player 1"] >= $games_needed_to_win
   puts "Player 1 wins!"
-elsif current_Score["Player 2"] >= games_needed_to_win
+elsif current_Score["Player 2"] >= $games_needed_to_win
   puts "Player 2 wins!"
 else
   next

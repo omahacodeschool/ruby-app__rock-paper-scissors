d444f440817b725ef706ca3017d1227cdd3f5e43
Jeremy Stoll <j8r8my@gmail.com>
2016-02-01 16:37:33 -0600

    Working, Still

--- lib/ruby-app__rock-paper-scissors.rb
+++ lib/ruby-app__rock-paper-scissors.rb
@@ -1,142 +1,202 @@
-#"initalize" the system by puts-ing non-functioning gobbledy-gook"
-def start_up
-end
+class RockPaperScissorsGame
 
-#pause for effect while program picks a move. Only "puts", with no calculating.
-def computing_pause
-  puts "The RPS Thinking Machine v1.3 is computing..."
-  sleep 2 
-  puts "gggggggggggggggggggg"
-  sleep 0.6
-  puts "gggggggggggggggggggg"
+  #displays the ascii image of the selected weapon
+  #takes in "r", "p", or "s"
+  #puts ascii plus the weapon name
+  #returns nil
+  def display_ascii(weapon)
+    if weapon == "r"
+      puts "          _______   "
+      puts "      ---'     |_)  "
+      puts "             |_|__) "
+      puts "            (_____) "
+      puts "            (____)  "
+      puts "      ---.__(___)   "
+      puts ""
+      puts "   ROCK"
 
-  sleep 0.5
-  puts "gggggggggggggggggggg"
+    elsif weapon == "p"
+      puts "          _______          "
+      puts "      ---'   ____)____     "
+      puts "                ______)    "
+      puts "                _______)   "
+      puts "               _______)    "
+      puts "      ---.__________)      "
+      puts ""
+      puts "   PAPER"
 
-  sleep 0.5
-  puts "gggggggggggggggggggg"
-  sleep 0.5
+    elsif weapon == "s"
+      puts "          _______          "
+      puts "      ---'   ____)____     "
+      puts "                ______)    "
+      puts "             __________)   "
+      puts "            (____)         "
+      puts "      ---.__(___)          "
+      puts ""
+      puts "    SCISSORS"
+      
+    end 
+  end
 
-  puts "gggggggggggggggggggg"
-  puts "..."
-  puts "________________________________________________________"
-  puts ""
-  puts "Computer picks:"
-end
 
 
-def display_ascii(x)
-  if x == "r"
-    puts "          _______   "
-    puts "      ---'     |_)  "
-    puts "             |_|__) "
-    puts "            (_____) "
-    puts "            (____)  "
-    puts "      ---.__(___)   "
+  #"initalize" the system by puts-ing non-functioning gobbledy-gook"
+  def start_up
+      puts "(INITIALIZING...)" 
+  end
+
+  #puts introduction and gets name, chomped and capitalized
+  #gets user_name
+  #saves user_name
+  def put_intro_and_get_username
+    puts "To play the RPS Thinking Machine v1.3, Please enter your name"
+    @user_name = gets.chomp.capitalize
+  end
+
+  #NEEDS WORK
+  #pause for effect while program picks a move. Only "puts", with no calculating.
+  def computing_pause
+    puts "The RPS Thinking Machine v1.3 is computing..."
+    sleep 0.3 
+    puts "9779546%#uu..."
+    puts "23cDFnstjGFHgcv4979%&6485"
+    puts "3597*))*(8yGTFUdufykbkuyYF)}"
+    sleep 0.6
+    puts "8JhfcSWk5764%#3erGFTfrvVCJTTXCJCvCJTjfc734..."
+
+    sleep 0.5
+    puts "ycjtCXTJckyjvVCJTTXCJCvCJTjvvk57bc64%#34575"
+
+    sleep 0.5
+    puts "DFnstjGDFnstjGFHg"
+    sleep 0.5
+
+    puts "dufyk01029bdtykbkuyYF)..."
+    puts "..."
+    puts "________________________________________________________"
+    sleep 0.2
     puts ""
-    puts "   ROCK"
+    puts "Computer picks:"
+  end
 
-  elsif x == "p"
-    puts "          _______          "
-    puts "      ---'   ____)____     "
-    puts "                ______)    "
-    puts "                _______)   "
-    puts "               _______)    "
-    puts "      ---.__________)      "
-    puts ""
-    puts "   PAPER"
+  #comp makes selection (1, 2, or 3), and returns the integer
+  def computer_picks_weapon
+    @computer_weapon_power = rand(1.. 3)
+  end
 
-  elsif x == "s"
-    puts "          _______          "
-    puts "      ---'   ____)____     "
-    puts "                ______)    "
-    puts "             __________)   "
-    puts "            (____)         "
-    puts "      ---.__(___)          "
-    puts ""
-    puts "    SCISSORS"
-    
+
+
+  #makes a comparison_set array as a key to compare user selection to the comp selection
+  #uses an "r", "p", or "s", representing the user's weapon (@user_weapon)
+  #sets (@comparison_set) to an array in the order of [losing weapon, tying weapon, winning weapon] and in the form of ["s", "r", "p"]
+  def mk_comparison_set
+    if @user_weapon == "r"
+      @comparison_set = ["s", "r", "p"]
+    elsif @user_weapon == "p"
+      @comparison_set = ["r", "p", "s"]
+    elsif @user_weapon == "s"
+      @comparison_set = ["p", "s", "r"]
+    end
+  end
+
+  #Translates comparison_set
+  #sets computer_weapon to a value in the form of "s", "r", or "p" using the comparison_array indexed against the randomly-selected integer in computer_picks_weapon
+  def translate_comparison_set_into_computer_weapon
+    @computer_weapon = @comparison_set[@computer_weapon_power]
   end 
-end
 
-#makes a comparison_set array as a key to compare user selection to the comp selection
-def mk_comparison_set(user_sel_rps)
-  if user_sel_rps == "r"
-    $comparison_set = ["s", "r", "p"]
-  elsif user_sel_rps == "p"
-    $comparison_set = ["r", "p", "s"]
-  elsif user_sel_rps == "s"
-    $comparison_set = ["p", "s", "r"]
+
+  #gets the outcome of the game and displays it, by comparing @computer_weapon_power against @user_weapon_power
+  #The user_weapon_power is always set to 2.
+  #returns "comp_win", "user_win", or "no_win"
+  def get_outcome
+  @user_weapon_power = 2
+    if @computer_weapon_power > @user_weapon_power
+      return "comp_win"
+    elsif @computer_weapon_power < @user_weapon_power
+      return "user_win"
+    else
+      return "no_win"
+    end
   end
-end
 
-#gets the outcome of the game and displays it
-def get_outcome(comp_i, user_i)
-  if comp_i > user_i
-    puts "COMP WINS!"
-    score
-  elsif comp_i < user_i
-    puts "USER WINS"
-  elsif comp_i == user_i
-    puts "TIE GAME!"
+
+  #NEEDS WORK
+  #stores the score in the all_games_info hash
+  #takes in a "c" for a computer win, or a "u" for a user win
+  #saves the value 
+  def make_score_for_game(winner)
+    if winner == "c"
+      @all_games_info << "c"
+    elsif winner == "u"
+      @all_games_info << "u"
+    end
   end
+
+  #puts the winner of the game
+  #takes in "comp_win", "user_win", or "no_win", and announces winner with "puts"
+  def puts_winner (result)
+    if result == "comp_win"
+      puts "COMPUTER WINS! Resistance is futile!"
+    elsif result == "user_win"
+      puts "USER WINS! You got lucky!"
+    else
+      puts "TIE GAME!"
+    end
+  end
+
+#get the number of games needed to win the set
+def get_number_of_games_needed_to_win_set
+  @number_of_games_needed_to_win_set = gets.chomp
 end
 
-#stores the score in the all_games_info hash
-def make_score_for_game(user,comp)
-  all_games_info
+#Check to see if the set of games is over by counting wins in the @all_games_info array and comparing them against the @number_of_games_needed_to_win_set
+def is_game_over_check? 
+  score_hash = {}
+  @all_games_info.uniq.each do |element|
+    score_hash[:"#{element}"] = "#{all_games_info.count(element)}"
+  end
+  if score_hash[:c] == @number_of_games_needed_to_win_set 
+    #Computer wins the set
+  elsif score_hash[:u] == @number_of_games_needed_to_win_set
+    #User wins the set
 end
 
-#intro to the game
-start_up
 
-#makes hashes for keeping score
-score_info = []
-all_games_info = {}
 
-#gets user_name
-puts "To play the RPS Thinking Machine v1.3, Please enter your name"
-$user_name = gets.chomp.capitalize
+  #________________________________ THIS RUNS A GAME
 
-#"initalize" the system by puts-ing non-functioning gobbledy-gook"
-# CALL the method here.
+  #intro to the game
+  start_up
 
-#gives option of moves (r,p, or s)
-puts "Ok, #{$user_name}! Please choose: Rock, Paper, or Scissors?"
-puts "You don't have to type it all out- just the first letter will work."
-
-#takes input (choice of rock, paper, or scissors) and only accepts first char.
-$user_sel_rps = gets.downcase.slice!(0)
-puts "You chose..."
-sleep 1.5
-
-#displays the user_choice in ascii
-display_ascii($user_sel_rps)
-puts ""
-sleep 0.5
-
-#inserts user_sel information keyed to 2 into game_info hash
-game_info[2] = $user_sel_rps
-
-#comp makes selection (1, 2, or 3)
-$computer_sel_rand = rand(1.. 3)
-
-#make an array of the hierarchy of objects with corresponding indexes showing loser, tie, and winner
-mk_comparison_set($user_sel_rps)
-
-#Translate the computer_sel_key into r, p, or s.
-$computer_sel_rps = $comparison_set[$computer_sel_rand] 
-
-#inserts comp_sel information into game_info hash and links with its key.
-game_info[$computer_sel_rand] = 
-
-#Translate selections into the two competing hand forms, and give integers
+  #NEEDS WORK
+  #makes hashes for keeping score
+  score_info = []
+  all_games_info = {}
+  game_info = {}
 
 
 
-puts $user_sel.to_s + " <= user"
+  #NEEDS WORK
+  #"initalize" the system by puts-ing non-functioning gobbledy-gook"
+  # CALL the method here.
 
-puts $computer_sel.to_s + " <= comp"
+  #gives option of moves (r,p, or s)
+  puts "Ok, #{@user_name}! Please choose: Rock, Paper, or Scissors?"
+  puts ""
+  puts "No need to type it all out- just the first letter will work."
+
+  #takes input (choice of rock, paper, or scissors) and only accepts first char.
+  @user_sel_rps = gets.downcase.slice!(0)
+  puts "You chose..."
+  sleep 0.2
+
+  #displays the user_choice in ascii
+  display_ascii($user_sel_rps)
+  puts ""
+  sleep 0.3
 
 
- 
+
+end
+   

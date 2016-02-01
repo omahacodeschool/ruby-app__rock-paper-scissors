0625a11f7ba44304f0ca54b0765966331beea494
Amy Wall <amymariewall@gmail.com>
2016-02-01 12:42:39 -0600

    RPS WIP 2

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -1,18 +1,18 @@
 require 'pry'
 
 #def rock_paper_scissors(str)
-  $player_1_score = 0
-  $player_2_score = 0
-  $player_1_name = ""
-  $player_2_name = ""
+  player_1_score = 0
+  player_2_score = 0
+  player_1_name = ""
+  player_2_name = ""
 
   puts "Player 1, please enter your name:"
-  $player_1_name = gets.chomp
-  puts "Hello, #{$player_1_name}!"
+  player_1_name = gets.chomp
+  puts "Hello, #{player_1_name}!"
 
   puts "Player 2, please enter your name:"
-  $player_2_name = gets.chomp
-  puts "Hello, #{$player_2_name}!"
+  player_2_name = gets.chomp
+  puts "Hello, #{player_2_name}!"
 
   puts "Player 1, enter your move:"
   player_1_move = gets.chomp.downcase
@@ -25,32 +25,32 @@ require 'pry'
 
   if player_1_move == 'paper'
     if player_2_move == 'rock'
-      puts "#{$player_1_name}, you win!"
-      $player_1_score += 1
+      puts "#{player_1_name}, you win!"
+      player_1_score += 1
     end
     if player_2_move == 'scissors'
-      puts "#{$player_2_name}, you win!"
-      $player_2_score += 1
+      puts "#{player_2_name}, you win!"
+      player_2_score += 1
     end
 
   elsif player_1_move == 'scissors'
     if player_2_move == 'rock'
-      puts "#{$player_2_name}, you win!"
+      puts "#{player_2_name}, you win!"
       $player_2_score += 1
     end
     if player_2_move == 'paper'
-      puts "#{$player_1_name} wins!"
-      $player_1_score +=1
+      puts "#{player_1_name} wins!"
+      player_1_score +=1
     end
 
   elsif player_1_move == 'rock'
     if player_2_move == 'paper'
-      puts "#{$player_2_name}, you win!"
-      $player_2_score += 1
+      puts "#{player_2_name}, you win!"
+      player_2_score += 1
     end
     if player_1_move == 'scissors'
-      puts "#{$player_1_name} wins!"
-      $player_1_score +=1
+      puts "#{player_1_name} wins!"
+      player_1_score +=1
     end
   end
 

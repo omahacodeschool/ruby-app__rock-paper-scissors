d7a723e136bc2b24eccdff023b9604463d2587fd
Michael Meier <mmeier61@yahoo.com>
2016-02-01 15:14:19 -0600

    Refactored

--- lib/PaperRockScissorsRefactored.rb
+++ lib/PaperRockScissorsRefactored.rb
@@ -11,18 +11,20 @@ end
 
 def player_setup1
      # until game == best_of do
-        puts "\nPlayer 1 please enter your move then pass the keyboard to your opponent, whats your move? (R)ock, (P)aper, or (S)cissors??"
-        return gets.chomp.upcase
-        system "clear"
+    puts "\nPlayer 1 please enter your move then pass the keyboard to your 
+    opponent, whats your move? (R)ock, (P)aper, or (S)cissors??"
+    return gets.chomp.upcase
+    system "clear"
 end
 
 def player_setup2
-        puts "\nPlayer 2 please enter your move then pass the keyboard to your opponent, whats your move? (R)ock, (P)aper, or (S)cissors??"
-        return gets.chomp.upcase
-        system "clear"
+    puts "\nPlayer 2 please enter your move then pass the keyboard to your o
+    pponent, whats your move? (R)ock, (P)aper, or (S)cissors??"
+    return gets.chomp.upcase
+    system "clear"
 end
 
-def RPS_Rules(player1,player2,player1_score,player2_score,tie_score)
+def RPS_Rules(player1,player2,player1_score,player2_score,tie_score,game)
     if
         ((player1 == 'R') && (player2 == 'S') ||
         (player1 == 'S') && (player2 == 'P')||
@@ -51,19 +53,18 @@ def final_output(player1_score,player2_score,best_of,tie_score)
     case
     when ((player1_score + player2_score + tie_score)) == best_of 
         if ((player1_score > player2_score))
-            # system "clear"
+            system "clear"
             return "Player 1 Wins!! Winning #{player1_score} out of #{best_of} games!!!"
         elsif ((player2_score > player1_score))
-            # system "clear"
+            system "clear"
             return "Player 2 Wins!!  Winning #{player2_score} out of #{best_of} games!!!"
         elsif ((player1_score == player2_score) || (tie_score + player1_score + player2_score == best_of))
-            # system "clear"
+            system "clear"
             return "Its a tie!!  Player 1 won #{player1_score} out of #{best_of} games, Player 2 won #{player2_score} out of #{best_of}games, and both Players tied for a total of #{tie_score} times!! What an even match we have here!!"
-    end    end
+        end    
+    end
 end
 best_of = intro
 player1 = player_setup1
 player2 = player_setup2
-# result = final_output(player1_score,player2_score,best_of,tie_score)
-# puts RPS_Rules(player1,player2,player1_score,player2_score,tie_score)
-# puts final_output(player1_score,player2_score,best_of,tie_score)
+puts final_output(player1_score,player2_score,best_of,tie_score)

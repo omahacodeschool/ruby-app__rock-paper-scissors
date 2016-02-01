e6e8a2749a5ae7e4c06f5ec35d48e7985a6c3498
Chris Foral <chris.foral@gmail.com>
2016-02-01 12:48:42 -0600

    Phase 1, Part 1

--- lib/rock-paper-scissors.rb
+++ lib/rock-paper-scissors.rb
@@ -10,38 +10,27 @@
 # alert players to who won the match
 #
 def rock_paper_scissors
-  win1 = 0
-  win2 = 0
-  
-    until win1 == 3 || win2 == 3
-      puts "Who dares enter the field of battle?"
-      player1 = gets.chomp.capitalize
-      puts "And who shall you test your fate against, #{player1}?"
-      player2 = gets.chomp.capitalize
-      puts "Very well, #{player1}, what is your weapon of choice: Rock, paper, or scissors?"
-      weapon1 = gets.chomp.downcase
-      puts "Now #{player2}, with what do you wish to vanquish #{player1}: Rock, paper, or scissors?"
-      weapon2 = gets.chomp.downcase
 
-      # player1 wins
-      if (weapon1 == "rock" && weapon2 == "scissors") || (weapon1 == "paper" && weapon2 == "rock") || (weapon1 == "scissors" && weapon2 == "paper")
-        puts "#{weapon1.capitalize} defeats #{weapon2}, and #{player1} vanquishes #{player2}!"
-        puts "For shame, #{player2}..."
-        win1 += 1
-      # player2 wins
-      elsif (weapon2 == "rock" && weapon1 == "scissors") || (weapon2 == "paper" && weapon1 == "rock") || (weapon2 == "scissors" && weapon1 == "paper")
-        puts "You chose wisely, #{player2}! #{weapon2.capitalize} is certainly mightier than the feeble #{weapon1}."
-        puts "You disgust me #{player1}."
-        win2 += 1
-      # tie game
-      elsif weapon1 == weapon2
-        puts "We have a tie! It seems #{player1} and #{player2} are indeed well matched... You both chose #{weapon1}."
-      end
-    end
-  if win1 == 3
-    puts "Congrats, #{player1}! You've won #{win1} matches."
-  elsif win2 == 3
-    puts "Congrats, #{player2}! You've won #{win2} matches."
+  puts "Who dares enter the field of battle?"
+  player1 = gets.chomp.capitalize
+  puts "And who shall you test your fate against, #{player1}?"
+  player2 = gets.chomp.capitalize
+  puts "Very well, #{player1}, what is your weapon of choice: Rock, paper, or scissors?"
+  weapon1 = gets.chomp.downcase
+  puts "Now #{player2}, with what do you wish to vanquish #{player1}: Rock, paper, or scissors?"
+  weapon2 = gets.chomp.downcase
+
+  # player1 wins
+  if (weapon1 == "rock" && weapon2 == "scissors") || (weapon1 == "paper" && weapon2 == "rock") || (weapon1 == "scissors" && weapon2 == "paper")
+    puts "#{weapon1.capitalize} defeats #{weapon2}, and #{player1} vanquishes #{player2}!"
+    puts "For shame, #{player2}..."
+  # player2 wins
+  elsif (weapon2 == "rock" && weapon1 == "scissors") || (weapon2 == "paper" && weapon1 == "rock") || (weapon2 == "scissors" && weapon1 == "paper")
+    puts "You chose wisely, #{player2}! #{weapon2.capitalize} is certainly mightier than the feeble #{weapon1}."
+    puts "You disgust me #{player1}."
+  # tie game
+  elsif weapon1 == weapon2
+    puts "We have a tie! It seems #{player1} and #{player2} are indeed well matched... You both chose #{weapon1}."
   end
 end
 

e0f960f9ff29b836014a9d2ee91bac290581743f
Patrick Clark <patclark1958@gmail.com>
2016-02-02 13:28:18 -0600

    testing multigame series

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -1,19 +1,17 @@
 
-
-
 class Game
 
-def setup
-  player_names
+  def setup
+    player_names
 
-  @player_1_name
-  @player_1_score = 0
+    @player_1_name
+    @player_1_score = 0
+  
+    @player_2_name
+    @player_2_score = 0
 
-  @player_2_name
-  @player_2_score = 0
-
-  # @player_victory = (determine_series_win + 1) / 2
-end
+    @player_victory = (how_many_games + 1) / 2
+  end
 
 # def run_game
 #   @p1_weapon = gets.chomp.downcase
@@ -22,68 +20,73 @@ end
     
 # end   
 
-# def end_of_series
-#     @player_1_score == @player_victory or @player_2_score == @player_victory
-# end 
+  def end_of_series
+    accumulate_series_score
+    if @player_1_score == @player_victory 
+        puts "Congratulations #{@player_1_name}"
+        puts "You have won this match #{@player_1_score} games to #{@player_2_score}"
+    elsif @player_2_score == @player_victory
+        puts "Congratulations #{@player_2_name}"
+        puts "You have won this match #{@player_2_score} games to #{@player_1_score}"
+    else puts "Play on."
+  end 
 
-# def accumulate_series_score(winner)
-#   if winner == 1
-#     return @player_1_score +=1
-#   elsif winner == 2
-#     return @player_2_score +=1
-# end
-
-# def determine_series_win
-#   puts "Please enter 1, 3, 5, or 7 to choose how many games you want to play."
-#     match_duration = gets.chomp.to_i
-    
-    #md = match_duration.to_i
-# end
-
-def player_names
-  puts "What name do you want to use, Player 1?"
-    @player_1_name = gets.chomp
-  puts "What name do you want to use, Player 2?"
-    @player_2_name = gets.chomp
-end
-
-
-def choose_weapons
-  puts "Choose 'r' for rock, 'p' for paper or 's' for scissors"
-    @weapon_p1 = gets.chomp.downcase
-   puts "Choose 'r' for rock, 'p' for paper or 's' for scissors" 
-    @weapon_p2 = gets.chomp.downcase
-end
-      
-def determine_winner_one_game
-  if @weapon_p1 == "r" && @weapon_p2 == "s"
-    return 1 
-  elsif @weapon_p1 == "s" && @weapon_p2 == "p"
-    return 1 
-  elsif @weapon_p1 == "p" && @weapon_p2 == "r"      
-    return 1 
-
-  elsif @weapon_p1 == "p" && @weapon_p2 == "s"
-    return 2
-  elsif @weapon_p1 == "r" && @weapon_p2 == "p"
-    return 2
-  elsif @weapon_p1 == "s" && @weapon_p2 == "r"
-    return 2  
-
-  else
-    return nil
-  end      
-end
-
-def display_winner_one_game(r)
-  if r == 1
-    puts "#{@player_1_name} wins"
-  elsif r == 2
-    puts "#{@player_2_name} wins"
-  else
-    puts "Tie"
+  def accumulate_series_score(winner)
+    if winner == 1
+      return @player_1_score +=1
+    elsif winner == 2
+      return @player_2_score +=1
   end
-end
+
+  def how_many_games
+    puts "Please enter 1, 3, 5, or 7 to choose how many games you want to play."
+    match_duration = gets.chomp.to_i
+  end
+
+  def player_names
+    puts "What name do you want to use, Player 1?"
+      @player_1_name = gets.chomp
+    puts "What name do you want to use, Player 2?"
+      @player_2_name = gets.chomp
+  end
+
+
+  def choose_weapons
+  puts "Choose 'r' for rock, 'p' for paper or 's' for scissors"
+      @weapon_p1 = gets.chomp.downcase
+     puts "Choose 'r' for rock, 'p' for paper or 's' for scissors" 
+      @weapon_p2 = gets.chomp.downcase
+  end
+      
+  def determine_winner_one_game
+    if @weapon_p1 == "r" && @weapon_p2 == "s"
+      return 1 
+    elsif @weapon_p1 == "s" && @weapon_p2 == "p"
+      return 1 
+    elsif @weapon_p1 == "p" && @weapon_p2 == "r"      
+      return 1 
+  
+    elsif @weapon_p1 == "p" && @weapon_p2 == "s"
+      return 2
+    elsif @weapon_p1 == "r" && @weapon_p2 == "p"
+      return 2
+    elsif @weapon_p1 == "s" && @weapon_p2 == "r"
+      return 2  
+
+    else
+      return nil
+    end      
+  end
+
+  def display_winner_one_game(r)
+    if r == 1
+      puts "#{@player_1_name} wins"
+    elsif r == 2
+      puts "#{@player_2_name} wins"
+    else
+      puts "Tie"
+    end
+  end
 
 end #this is the end of class
 
@@ -92,6 +95,7 @@ new_game.setup
 new_game.choose_weapons
 result = new_game.determine_winner_one_game
 new_game.display_winner_one_game(result)
+new_game.
 
 
 # def declare_series_winner

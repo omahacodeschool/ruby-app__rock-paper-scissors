ab7e4aa4c11f78b8f3f48c90d27ce8a52a9f20d7
Bradley Biehl <bradley.biehl@gmail.com>
2016-02-02 16:28:00 -0600

    Phase 2 refactor and TomDoc

--- lib/rock-paper-scissors.rb
+++ lib/rock-paper-scissors.rb
@@ -8,7 +8,7 @@
 # => Player 1 types "rock" and hits enter
 # gets.chomp => "rock"
 #
-# Returns the weapon ("rock"", "paper", or "scissors") of Player 1 as a String
+# Returns a String ("rock"", "paper", or "scissors")
 #
 def get_player_1_weapon
   puts "Player 1, unleash your weapon:"
@@ -25,7 +25,7 @@ end
 # => Player 2 types "paper" and hits enter
 # gets.chomp => "paper"
 #
-# Returns the weapon ("rock"", "paper", or "scissors") of Player 2 as a String
+# Returns a String ("rock"", "paper", or "scissors")
 #
 def get_player_2_weapon
   puts "Player 2, unleash your weapon:"
@@ -34,32 +34,31 @@ end
 
 # Defines how the game was won
 #
-# Arguments:
-# String - weapon of player 1 ("rock"", "paper", or "scissors")
-# String - weapon of player 2 ("rock", "paper", or "scissors")
+# weapon_p1 - String that is Player 1 weapon ("rock", "paper", or "scissors")
+# weapon_p2 - String that is Player 2 weapon ("rock", "paper", or "scissors")
 #
 # Examples:
 #
-# Player 1 weapon is "paper" and player 2 weapon is "rock"
-# => 1 (1 will later need to be defined to be equal to "Paper Covers Rock")
+# how_game_was_won("paper", "rock")
+# => 1
 #
-# Player 1 weapon is "paper" and player 2 weapon is "scissors"
-# => 2 (2 will later need to be defined to be equal to "Scissors Cut Paper")
+# how_game_was_won("scissors", "paper")
+# => 2
 #
-# Player 1 weapon is "rock" and player 2 weapon is "scissors"
-# => 3 (3 will later need to be defined to be equal to "Rock Smashes Scissors")
+# how_game_was_won("rock", "scissors")
+# => 3
 #
-# Player 1 and Player 2 weapons are the same
-# => nil (nil will later need to be inherently defined as a "draw")
+# how_game_was_won("rock", "rock")
+# => 0
 #
-# Returns an integer value of 0, 1, 2, or 3
+# Returns an Integer value of 0, 1, 2, or 3
 #
 def how_game_was_won(weapon_p1, weapon_p2)
   if weapon_p1 == "paper" && weapon_p2 == "rock"
     return 1
   elsif weapon_p2 == "paper" && weapon_p1 == "rock"
     return 1
-  elsif apple == "scissors" && weapon_p2 == "paper"
+  elsif weapon_p1 == "scissors" && weapon_p2 == "paper"
     return 2
   elsif weapon_p2 == "scissors" && weapon_p1 == "paper"
     return 2
@@ -74,22 +73,21 @@ end
 
 # Defines which player won the game
 #
-# Arguments(2)
-# String - Player 1 weapon ("rock", "paper", or "scissors")
-# String - Player 2 weapon ("rock", "paper", or "scissors")
+# weapon_p1 - String that is Player 1 weapon ("rock", "paper", or "scissors")
+# weapon_p2 - String that is Player 2 weapon ("rock", "paper", or "scissors")
 #
 # Examples:
 #
-# Player 1 weapon is "rock" and Player 2 weapon is "scissors"
-# => 1 (will later need to be defined as a Player 1 win)
+# game_winner("rock", "scissors")
+# => 1
 #
-# Player 1 weapon is "rock" and Player 2 weapon is "paper"
-# => 2 (will later need to be defined as a Player 2 win)
+# game_winner("scissors", "rock")
+# => 2
 #
-# Player 1 weapon is the same as Player 2 weapon
-# => nil (will later need to be inherently defined as a draw)
+# game_winner("paper", "paper")
+# => nil
 #
-# Returns an integer value of 1, 2, 3 or nil
+# Returns an Integer value of 1, 2, 3 or nil
 #
 def game_winner(weapon_p1, weapon_p2)
   if weapon_p1 == "rock" && weapon_p2 == "scissors"
@@ -102,7 +100,7 @@ def game_winner(weapon_p1, weapon_p2)
     return 2
   elsif weapon_p2 == "paper" && weapon_p1 == "rock"
     return 2
-  elsif weapon_p2 == "scissors" && weapon_p2 == "paper"
+  elsif weapon_p2 == "scissors" && weapon_p1 == "paper"
     return 2
   else
     return nil
@@ -111,26 +109,25 @@ end
 
 # Defines what to display based on how the game was won
 #
-# Arguments(3)
-# Integer - value from how_game_was_won method (0, 1, 2, or 3)
-# String - Player 1 weapon ("rock", "paper", or "scissors")
-# String - Player 2 weapon ("rock", "paper", or "scissors")
+# won - an Integer value from how_game_was_won method (0, 1, 2, or 3)
+# weapon_p1 - String that is Player 1 weapon ("rock", "paper", or "scissors")
+# weapon_p2 - String that is Player 2 weapon ("rock", "paper", or "scissors")
 #
 # Examples:
 #
-# Integer value from how_game_was_won method == 1
-# => "Paper Covers Rock" (both String arguments are disregarded)
+# display_how_game_won(1, "rock", "paper")
+# => "Paper Covers Rock" (both String arguments are not needed)
 #
-# Integer value from how_game_was_won method == 2
-# => "Scissors Cut Paper" (both String arguments are disregarded)
+# display_how_game_won(2, "paper", "scissors")
+# => "Scissors Cut Paper" (both String arguments are not needed)
 #
-# Integer value from how_game_was_won method == 3
+# display_how_game_won(3, "rock", "scissors")
 # => "Rock Smashes Scissors" (both String arguments are disregarded)
 #
-# Integer value from how_game_was_won method == 4
-# => "Player 1 weapon equals Player 2 weapon" (String arguments are used)
+# display_how_game_won(0, "rock", "rock")
+# => "rock equals rock"
 #
-# Returns a statement on how the game was won
+# Returns a String describing how the game was won or drawn
 #
 def display_how_game_won(won, weapon_p1, weapon_p2)
   if won == 1
@@ -146,21 +143,20 @@ end
     
 # Defines what to display based on which player won the game
 #
-# Arguments
-# Integer - value from game_winner method (1, 2, 3, or nil)  
+# winner - Integer value (1, 2, or nil) from game_winner method 
 #
 # Examples:
 #
-# Integer value from game_winner method == 1
+# display_game_result(1)
 # => "Player 1 wins!"
 #
-# Integer value from game_winner method == 2
+# display_game_result(2)
 # => "Player 2 wins!"  
 #
-# Nil value from game_winner method
+# display_game_result(nil)
 # => "It's a draw!"
 #
-# Returns a statement on which player won the game
+# Returns a String defining which player won the game, or if the game was drawn
 #
 def display_game_result(winner)
   if winner == 1

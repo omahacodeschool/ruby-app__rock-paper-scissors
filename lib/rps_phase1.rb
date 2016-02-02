3a4fc6d63b0fa556770fc55feb51f28b380c7cc1
Jillian Waggoner <jillianwaggoner@icloud.com>
2016-02-02 11:41:47 -0600

    thru phase 1 part 2

--- lib/rps_phase1.rb
+++ lib/rps_phase1.rb
@@ -1,30 +1,35 @@
 puts "How many games of RPS must be won in order to declare a winner?"
-games_reqd = gets.chomp
+games_reqd = gets.chomp.to_i
 
-games_remaining = 1
-  while games_remaining < games_reqd
-    ??????
-    games_remaining = games_remaining + 1
+games_remaining = 0
+p1_wins = 0
+p2_wins = 0
+while games_remaining < games_reqd
+games_remaining = games_remaining += 1
+  
+  puts "Player 1: rock, paper or scissors?"
+  p1_move = gets.chomp.downcase
+
+  puts "Player 2: rock, paper or scissors?"
+  p2_move = gets.chomp.downcase
+
+  if (p1_move == "rock" && p2_move == "scissors")
+    p1_wins += 1
+  elsif (p1_move == "rock" && p2_move == "paper")
+    p2_wins += 1
+  elsif (p1_move == "paper" && p2_move == "rock")
+    p1_wins += 1
+  elsif (p1_move == "paper" && p2_move == "scissors")
+    p2_wins += 1
+  elsif (p1_move == "scissors" && p2_move == "paper")
+    p1_wins += 1
+  elsif (p1_move == "scissors" && p2_move == "rock")
+    p2_wins += 1
   end
+end
 
-puts "Player 1: rock, paper or scissors?"
-p1_move = gets.chomp.downcase
-
-puts "Player 2: rock, paper or scissors?"
-p2_move = gets.chomp.downcase
-
-if (p1_move == "rock" && p2_move == "scissors")
-  puts "Player 1 wins!"
-elsif (p1_move == "rock" && p2_move == "paper")
-  puts "Player 2 wins!"
-elsif (p1_move == "paper" && p2_move == "rock")
-  puts "Player 1 wins!"
-elsif (p1_move == "paper" && p2_move == "scissors")
-  puts "Player 2 wins!"
-elsif (p1_move == "scissors" && p2_move == "paper")
-  puts "Player 1 wins!"
-elsif (p1_move == "scissors" && p2_move == "rock")
-  puts "Player 2 wins!"
+if p1_wins > p2_wins
+  puts "Players 1 wins!"
 else
-  puts "That was a draw!"
+  puts "Player 2 wins!"
 end

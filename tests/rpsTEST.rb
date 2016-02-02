39e0683c2fbe23348fc36056976b862550772b07
Michael Meier <mmeier61@yahoo.com>
2016-02-02 15:51:42 -0600

    Moved

--- tests/rpsTEST.rb
+++ tests/rpsTEST.rb
@@ -2,28 +2,28 @@ require_relative 'test_helper.rb'
 require_relative 'paperrockscissorsrefactored.rb'
 
 class RPStest < Minitest::Test
-  def test_rock1
+  def rock_test1
     test = Game.new
     player1 = "P"
     player2 = "R"
     assert_equal("player1",test.rps_rules("P","R"))
   end
 
-  def test_rock2
+  def rock_test2
     test = Game.new
     player1 = "R"
     player2 = "P"
     assert_equal("player2", test.rps_rules("R","P"))
   end
 
-  def test_rock3
+  def rock_test3
     test = Game.new
     player1 = "S"
     player2 = "P"
     assert_equal("player1", test.rps_rules("S","P"))
   end
 
-  def test_rock4
+  def rock_test3
     test = Game.new
     player1 = "S"
     player2 = "S"

98dcc97c352f00e54327dc1dc4d5c2e565d51206
Jillian Waggoner <jillianwaggoner@icloud.com>
2016-02-01 18:51:21 -0600

    WIP

--- lib/rps_phase1.rb
+++ lib/rps_phase1.rb
@@ -1,8 +1,17 @@
+puts "How many games of RPS must be won in order to declare a winner?"
+games_reqd = gets.chomp
+
+games_remaining = 1
+  while games_remaining < games_reqd
+    ??????
+    games_remaining = games_remaining + 1
+  end
+
 puts "Player 1: rock, paper or scissors?"
-p1_move = gets.chomp
+p1_move = gets.chomp.downcase
 
 puts "Player 2: rock, paper or scissors?"
-p2_move = gets.chomp
+p2_move = gets.chomp.downcase
 
 if (p1_move == "rock" && p2_move == "scissors")
   puts "Player 1 wins!"

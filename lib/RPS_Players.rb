1f880b78d4505b9ff4ed9b5a2c1e51bdb2ae311e
Meagan Jungman <meagan.jungman@gmail.com>
2016-02-03 12:53:19 -0600

    TomDoc notes added.

--- lib/RPS_Players.rb
+++ lib/RPS_Players.rb
@@ -2,6 +2,13 @@
 
  class Players
 
+  # @player_name: Instance Variable with String Value. stores Player One's Name
+  # @other_player_name: Instance Variable with String value. stores Player Two's name
+  # @max_games: Instance Variable with Integer Value. Stores max number of RPS games to play
+  # @played_games: Instance Variable with Integer Value. Keeps track of how many games have been played. 
+  # @weapon: Instance Variable with String Value. Contains player one's weapon selection.
+  # @other_weapon: Instance Variable with String Value. Contains player two's weapon selection.
+  # @battle_result: Instance Variable with Integer Value. Used with methods to determine the winner of an RPS game.
   def initialize()
     @player_name = ""
     @other_player_name = ""
@@ -12,49 +19,62 @@
     @battle_result = 0
   end
 
+  # SETS user String input to value of max number of RPS games to play. Converts to Integer
   def set_max_games()
     puts "Let's play 'Rock, Paper Scissors!'"
     print "How many games do you want to play?  "
     @max_games = gets.chomp.to_i
   end
 
-  def set_played_games()
-    @played_games += 1
-  end
-
-  def get_played_games()
-    return @played_games
-  end
-
+ # RETURNS Integer value of Instance Variable @max_games (total number of games to be played)
   def get_max_games()
     return @max_games
   end
 
- def set_name()
+  # SETS Integer defining number of games that have been finished by players. 
+  # Adds +1 to @played_games current value each time method is called.
+  def set_played_games()
+    @played_games += 1
+  end
+
+  # RETURNS Integer value of Instance Variable @played_games (Games completed by players)
+  def get_played_games()
+    return @played_games
+  end
+
+  # SET String containing Player One's name, based on user input.
+  def set_name()
     print "PLAYER ONE, enter your name:  "
     @player_name = gets.chomp
   end
 
+  # RETURN String value of Instance Variable @player_name (Player One's name)
   def get_name()
     return @player_name
   end
 
-
-   def set_other_name()
+  # SET String containing Player Two's name, based on user input.
+  def set_other_name()
      print "PLAYER TWO, enter your name:  "
      @other_player_name = gets.chomp
   end
 
+  # RETURN String value of Instance Variable @other_player_name (Player Two's name)
   def get_other_name()
     return @other_player_name
   end
 
-
+  # Method RETURNS set_name Method, followed by set_other_name Method, in order to set the name Strings of both players. 
   def name_all_players()
     set_name()
     set_other_name()
   end
 
+  # Method SETS String containing Player One's weapon selection--based on user input.
+  # weapon: local variable with String value. Contains user input
+  # case weapon: Case Conditional to check value of local variable.
+    # If user input does not match available options, it will put a string message
+    # and trigger the prompt to select a weapon again, by setting the value of weapon equal to the set_weapon() Method.
   def set_weapon()
     print "#{@player_name}, what is your weapon of choice?  "
     weapon = gets.chomp.downcase
@@ -76,18 +96,18 @@
     end
    
     @weapon = weapon
-
   end
 
-  # DOES: 
-  #
-  # @weapon
-  #
-  # Returns String
+  # RETURN String value of Instance Variable @weapon (Player One's Weapon)
   def get_weapon()
     return @weapon
   end
 
+  # Method SETS String containing Player Two's weapon selection--based on user input.
+  # other_weapon: local variable with String value. Contains user input
+  # case other_weapon: Case Conditional to check value of local variable.
+    # If user input does not match available options, it will put a string message
+    # and trigger the prompt to select a weapon again, by setting the value of other_weapon equal to the set__other_weapon() Method.
   def set_other_weapon()
     print "#{@other_player_name}, what is your weapon of choice?  "
     other_weapon = gets.chomp.downcase
@@ -111,30 +131,36 @@
     @other_weapon = other_weapon
   end
 
+  # RETURN String value of Instance Variable @other_weapon (Player Two's Weapon)
   def get_other_weapon()
     return @other_weapon
   end
 
+  # Method calls set_weapon Method, followed by set_other_weapon Method
+  # to set a new String value to each corresponding instance variable. 
   def reset_player_weapons()
-   set_weapon()
-   set_other_weapon()
+     set_weapon()
+     set_other_weapon()
   end
 
+  # Alorithmic Method using Conditional to compare player weapons. SETS Integer value based on outcome of the conditional.
+  # 1 = Player One Wins, 2 = Player Two Wins, 0 = Tie Game 
   def set_battle()
-  if ((@weapon == "scissors") && (@other_weapon == "paper")) || ((@weapon == "rock") &&  (@other_weapon == "scissors")) || ((@weapon == "paper") && (@other_weapon == "rock"))
-    @battle_result = 1
-  
-  elsif ((@weapon == "rock") && (@other_weapon == "paper")) || ((@weapon == "scissors") && (@other_weapon == "rock")) || ((@weapon == "paper") && (@other_weapon == "scissors"))
-    @battle_result = 2 
+    if ((@weapon == "scissors") && (@other_weapon == "paper")) || ((@weapon == "rock") &&  (@other_weapon == "scissors")) || ((@weapon == "paper") && (@other_weapon == "rock"))
+      @battle_result = 1
+    
+    elsif ((@weapon == "rock") && (@other_weapon == "paper")) || ((@weapon == "scissors") && (@other_weapon == "rock")) || ((@weapon == "paper") && (@other_weapon == "scissors"))
+      @battle_result = 2 
 
-  else
-    @weapon == @other_weapon 
-    @battle_result = 0
-  end
+    else
+      @weapon == @other_weapon 
+      @battle_result = 0
+    end
 end
 
+# RETURN Integer value of Instance Variable @battle_result (Determines which player won an RPS match)
 def get_battle()
-  return @battle_result
+    return @battle_result
 end
 
 end
--- lib/RPS_score_count.rb
+++ lib/RPS_score_count.rb
@@ -1,5 +1,9 @@
 class Score_count
 
+  # @score_count: Instance Variable assigned an Integer value used to track the number of games won by Player One.
+  # @games_tied: Isntance Variable assigned an Integer value used to track the number of tied games.
+  # @other_score_count: Instance Variable assigned an Integer value used to track the number of games won by Player Two.
+  # @set_winner: Instance Variable assigned an Integer value based on the outcome of set_determine_set_winner Method.
   def initialize()
     @score_count = 0
     @games_tied = 0
@@ -7,40 +11,48 @@ class Score_count
     @set_winner = 0
   end
 
+  # Method increases Integer value of Instance Variable @score_count by 1 
+  # Increases number of games won for Player One
   def set_games_won()
     @score_count += 1
   end
 
-  def set_other_games_won()
-     @other_score_count += 1
-  end
-
-  # DOES: 
-  #
-  # @score_count: 
-  #
-  # Returns Integer
+  # RETURNS Integer value of Instance Variable @score_count (Games won by Player One)
   def get_games_won()
     return @score_count
   end
 
+  # Method increases Integer value of Instance Variable @other_score_count by 1 
+  # Increases number of games won for Player Two
+  def set_other_games_won()
+     @other_score_count += 1
+  end
+
+  # RETURNS Integer value of Instance Variable @other_score_count (Games won by Player Two)
   def get_other_games_won()
      return @other_score_count
   end
 
+  # Method increases Integer value of Instance Variable @games_tied by 1 
+  # Increases the number of tied games.
   def set_games_tied()
     @games_tied += 1
   end
 
-  # DOES: 
-  #
-  # @games_tied:
-  #
-  # Returns Integer
+  # RETURNS Integer value of Instance Variable @games_tied (Games tied by players)
   def get_games_tied()
     return @games_tied
   end
 
+  # algorithmic Method usng Conditional to compare the number of games won by each player
+  # as well as the number of tied games. SETS an Integer value based on this. 
+  #
+  # @score_count: Instance Variable. value = Integer used to track the number of games won by Player One.
+  # @other_score_count: Instance Variable. value = Integer used to track the number of games won by Player Two.
+  # @set_winner: Instance Variable assigned an Integer value based on the outcome of the conditional statement
+  #  1 = Player One Wins, 2 = Player Two Wins, 0 = Tie Game 
+  #
+  # SETS Integer Value for @set_winner Instance Variable. 
   def set_determine_set_winner()
     if @score_count > @other_score_count
           @set_winner = 1
@@ -53,6 +65,7 @@ class Score_count
     end
   end
 
+ # RETURNS Integer Value for @set_winner Instance Variable. 
   def get_determine_set_winner()
    return @set_winner
   end

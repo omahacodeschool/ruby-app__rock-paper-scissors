30d432ea96587d0eee42ba4af09955259689faa0
Tyler <tfisher322@gmail.com>
2016-02-03 15:20:39 -0600

    Refactoring_two

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -1,9 +1,4 @@
 i = 0
-p1_wins = 0
-p2_wins = 0
-r = "rock"
-p = "paper"
-s = "scissors"
 
 def ask_for_first_player_weapon
     puts "Player one, choose your weapon."
@@ -28,58 +23,55 @@ while i < rounds.to_i do
         weapon_p2 = ask_for_second_player_weapon
         i += 1
 puts "\n^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ "
-    if weapon_p1 == r && weapon_p2 == s
-        puts "Player 1 chose rock. \nPlayer 2 chose scissors."
-        puts "Rock beats scissors, Player 1 wins the round."
-        p1_wins += 1
-    end
-    if weapon_p1 == p && weapon_p2 == r
-        puts "Player 1 chose paper. \nPlayer 2 chose rock."
-        puts "Paper beats rock, Player 1 wins the round."
-        p1_wins += 1
-    end
-    if weapon_p1 == s && weapon_p2 == p
-        puts "Player 1 chose scissors. \nPlayer 2 chose rock."
-        puts "Scissors beats paper, Player 1 wins the round."
-        p1_wins += 1
-    end
 
-    if weapon_p1 == s && weapon_p2 == r
-        puts "Player 1 chose scissors. \n Player 2 chose rock."
-        puts "Rock beats scissors, Player 2 wins the round."
-        p2_wins += 1
-    end
-    if weapon_p1 == r && weapon_p2 == p
-        puts "Player 1 chose rock. \nPlayer 2 chose paper."
-        puts "Paper beats rock, Player 2 wins the round."
-        p2_wins += 1
-    end
+    def update_palyer_win_totals
     
-    if weapon_p1 == p && weapon_p2 == s
-        puts "Player 1 chose paper. \nPlayer 2 chose scissors."
-        puts "Scissors beats paper, Player 2 wins the round."
-        p2_wins += 1
-    end
-    
-    if weapon_p1 == r && weapon_p2 == r
-        puts "Player 1 chose rock. \nPlayer 2 chose rock."
-        puts "Both chose the same. Tie!"
-        p1_wins += 0
-        p2_wins += 0
-    end
-    
-    if weapon_p1 == p && weapon_p2 == p
-        puts "Player 1 chose paper. \nPlayer 2 chose paper."
-        puts "Both chose the same. Tie!"
-        p1_wins += 0
-        p2_wins += 0
-    end
-    
-    if weapon_p1 == s && weapon_p2 == s
-        puts "Player 1 chose scissors. \nPlayer 2 chose scissors."
-        puts "Both chose the same. Tie!"
-        p1_wins += 0
-        p2_wins += 0
+    p1_wins = 0
+    p2_wins = 0
+    r = "rock"
+    p = "paper"
+    s = "scissors"
+   
+        if weapon_p1 == r && weapon_p2 == s
+            puts "Player 1 chose rock. \nPlayer 2 chose scissors."
+            puts "Rock beats scissors, Player 1 wins the round."
+            p1_wins += 1
+        elsif weapon_p1 == p && weapon_p2 == r
+            puts "Player 1 chose paper. \nPlayer 2 chose rock."
+            puts "Paper beats rock, Player 1 wins the round."
+            p1_wins += 1
+        elsif weapon_p1 == s && weapon_p2 == p
+            puts "Player 1 chose scissors. \nPlayer 2 chose rock."
+            puts "Scissors beats paper, Player 1 wins the round."
+            p1_wins += 1
+        elsif weapon_p1 == s && weapon_p2 == r
+            puts "Player 1 chose scissors. \n Player 2 chose rock."
+            puts "Rock beats scissors, Player 2 wins the round."
+            p2_wins += 1
+        elsif weapon_p1 == r && weapon_p2 == p
+            puts "Player 1 chose rock. \nPlayer 2 chose paper."
+            puts "Paper beats rock, Player 2 wins the round."
+            p2_wins += 1
+        elsif weapon_p1 == p && weapon_p2 == s
+            puts "Player 1 chose paper. \nPlayer 2 chose scissors."
+            puts "Scissors beats paper, Player 2 wins the round."
+            p2_wins += 1
+        elsif weapon_p1 == r && weapon_p2 == r
+            puts "Player 1 chose rock. \nPlayer 2 chose rock."
+            puts "Both chose the same. Tie!"
+            p1_wins += 0
+            p2_wins += 0
+        elsif weapon_p1 == p && weapon_p2 == p
+            puts "Player 1 chose paper. \nPlayer 2 chose paper."
+            puts "Both chose the same. Tie!"
+            p1_wins += 0
+            p2_wins += 0
+        elsif weapon_p1 == s && weapon_p2 == s
+            puts "Player 1 chose scissors. \nPlayer 2 chose scissors."
+            puts "Both chose the same. Tie!"
+            p1_wins += 0
+            p2_wins += 0
+        end
     end
 puts "\n----------------------------------------------------------------------"
 end

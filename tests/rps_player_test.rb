7c49e317cc7d991fc1c685cb1c9be5c93773ff96
Meagan Jungman <meagan.jungman@gmail.com>
2016-02-03 15:28:59 -0600

    having trouble understanding how best to account for variables when writing tests for classes

--- tests/rps_player_test.rb
+++ tests/rps_player_test.rb
@@ -1,7 +1,5 @@
 require 'test_helper'
-require 'RPS_Game'
 require 'RPS_Players'
-require 'RPS_score_count'
 
 class RPSPlayerTest < Minitest::Test
  
@@ -9,7 +7,7 @@ class RPSPlayerTest < Minitest::Test
     testplayers = Players.new()
     testplayers.set_weapon_for_tests("rock")
     testplayers.set_weapon_for_tests("paper")
-    testplayers.set_battle_result_for_tests(0)
+    testplayers.set_battle_result_for_tests(2)
     testplayers.set_battle()
     assert_equal(2, testplayers.get_battle())
   end

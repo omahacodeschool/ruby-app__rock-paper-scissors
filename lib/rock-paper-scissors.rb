e02b57f5fe7aea6f2f65c5bedfd44462565db4f2
Courtney Burns <courtneylynnburns@gmail.com>
2016-02-03 13:27:19 -0600

    class work

--- lib/rock-paper-scissors.rb
+++ lib/rock-paper-scissors.rb
@@ -1,36 +1,8 @@
-require_relative 'game_return_methods'
-require_relative 'game_puts_methods'
-
-# Defines when weapon is misspelled by player 1
-#
-# $player_1_weapon, $player_2_weapon - A string of anything other than "rock","paper", or "scissors"
-# Returns True or False
-def p1_misspelled_weapon
-  ($player_1_weapon == "paper") || ($player_1_weapon == "rock") || ($player_1_weapon == "scissors")
-end
-
-# Defines when weapon is misspelled by player 2
-#
-# $player_1_weapon, $player_2_weapon - A string of anything other than "rock","paper", or "scissors"
-# Returns True or False
-def p2_misspelled_weapon
-  ($player_2_weapon == "paper") || ($player_2_weapon == "rock") || ($player_2_weapon == "scissors")
-end
-
-def respell_weapon_text
-  puts "Make sure you spelled that right and try again"
-  gets.chomp
-end
-
-
-
-
-$p1_wins = 0
-$p2_wins = 0
+require_relative 'game_methods'
 
 match = get_number_sets
 
-while $p1_wins + $p2_wins != match
+while sets_played != match
 
 $player_1_weapon = get_p1_weapon
 
---
+++ lib/rps-oop.rb
@@ -0,0 +1,9 @@
+require_relative 'game_methods'
+
+rps1 = Game.new
+
+rps1.get_number_sets
+rps1.weapon_for_player_1
+rps1.weapon_for_player_2
+rps1.play_game
+rps1.who_won

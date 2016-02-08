b8149c8cb516c234b973577b1a427833488d26d4
Tray Harrington <traylon.harrington@gmail.com>
2016-02-08 16:47:00 -0600

    Lizard Spock Functionality

--- lib/rock-paper-scissors.rb
+++ lib/rock-paper-scissors.rb
@@ -1,135 +1,156 @@
-def set_intial_values
-  $which_game = 1
-  $player1_wins = 0
-  $player2_wins = 0
-end
-
-def welcome
-  puts "Let's play Rock-Paper-Scissors!"
-  puts "\nHow many games would you like to play?"
-  gets.chomp.to_i
-end
-
-def get_total_games
-
-  $total_games = welcome
-    
-  if $total_games == 2
-    $wins_needed = 1
-  else
-    $wins_needed  = ($total_games / 2) + 1
+class Game
+  def initialize
+    @which_game = 1
+    @player1_wins = 0
+    @player2_wins = 0
   end
 
-  puts "\nGreat! Best #{$wins_needed} out of #{$total_games}!"
-end
-
-def game_format_setup
-  puts "--------------------------------------------------------------------"
-  puts "\nGame #{$which_game}! Commence!\n"
-end  
-
-def intermediary_message
-  puts "\nOkay! Player1, hand Player 2 the computer and look away."
-end
-
-def get_player1_move
-  puts "\nPlayer 1: What's your move?"
-  $player1_move = gets.chomp.capitalize
-  intermediary_message
-end
-
-def get_player2_move
-  puts "\nPlayer 2: What's your move?"
-  $player2_move = gets.chomp.capitalize
-end
-
-def determine_game_winner
-
-  if $player1_move == "Rock"
-      if $player2_move == "Paper"
-        return 2
-      elsif $player2_move == "Scissors"
-        return 1
-      else
-        return nil
-      end
-    elsif $player1_move == "Paper"
-      if $player2_move == "Scissors"
-        return 2
-      elsif $player2_move == "Rock"
-        return 1
-        player1_
-      else
-        return nil 
-      end
-    elsif $player1_move == "Scissors"
-      if $player2_move == "Rock"
-        return 2
-        player2_
-      elsif $player2_move == "Paper"
-        return 1
-        player1_
-      else
-        return nil
-        
-      end  
-    end    
-end
-
-def track_player_wins(num)
-
-  if num == 1
-    $player1_wins += 1
-  elsif num == 2
-    $player2_wins += 1
+  def welcome
+    puts "Let's play Rock-Paper-Scissors!"
+    puts "\nHow many games would you like to play?"
+    gets.chomp.to_i
   end
-end
 
-def game_outcomes(num)
-  if num == 1
-    puts "\n#{$player1_move} beats #{$player2_move}! Player 1 Wins!"
-    puts "--------------------------------------------------------------------"
-  elsif num == 2
-    puts "\n#{$player2_move} beats #{$player1_move}! Player 2 Wins!"
-    puts "--------------------------------------------------------------------"
-  else
-    puts "\nIt's a Tie! Get Ready for Game #{($which_game + 1)}!"
-    puts "--------------------------------------------------------------------"
-  end
-end
+  def get_total_games
 
-
-def display_set_winner
-  if $player1_wins > $player2_wins
-      puts "\nCongratulations Player 1! You take the cake with a score of #{$player1_wins} to #{$player2_wins}!"
-    elsif $player2_wins > $player1_wins
-      puts "\nCongratulations Player 2! You take the cake with a score of #{$player2_wins} to #{$player1_wins}!"
+    @total_games = welcome
+      
+    if @total_games == 2
+      @wins_needed = 1
     else
-      puts "\nWhat a grueling battle with no clear winner! Care to settle the score?"
-  end
-end
-
-def rock_paper_scissors
-
-  set_intial_values
-  get_total_games
-  
-  while $which_game <= $total_games
-    
-    if $player1_wins == $wins_needed || $player2_wins == $wins_needed
-        break
+      @wins_needed  = (@total_games / 2) + 1
     end
 
-    game_format_setup  
-    get_player1_move
-    get_player2_move
-    
-    winner = determine_game_winner
-    track_player_wins(winner)
-    game_outcomes(winner)
-
-    $which_game += 1
+    puts "\nGreat! Best #{@wins_needed} out of #{@total_games}!"
   end
 
-  display_set_winner       
+  def game_format_setup
+    puts "--------------------------------------------------------------------"
+    puts "\nGame #{@which_game}! Commence!\n"
+  end  
+
+  def intermediary_message
+    puts "\nOkay! Player1, hand Player 2 the computer and look away."
+  end
+
+  def get_player1_move
+    puts "\nPlayer 1: What's your move?"
+    @player1_move = gets.chomp.capitalize
+    intermediary_message
+  end
+
+  def set_player1_move(x)
+    @player1_move = x
+  end
+     
+  def set_player2_move(x)
+    @player2_move = x
+  end
+  
+  def get_player2_move
+    puts "\nPlayer 2: What's your move?"
+    @player2_move = gets.chomp.capitalize
+  end
+
+  def determine_game_winner
+
+    if @player1_move == "Rock"
+        if @player2_move == "Paper" || @player2_move == "Spock"
+          return 2
+        elsif @player2_move == "Scissors" || @player2_move == "Lizard"
+          return 1
+        else
+          return nil
+        end
+      elsif @player1_move == "Paper"
+        if @player2_move == "Scissors" || @player2_move == "Lizard"
+          return 2
+        elsif @player2_move == "Rock" || @player2_move == "Spock"
+          return 1
+        else
+          return nil 
+        end
+      elsif @player1_move == "Scissors"
+        if @player2_move == "Rock" || @player2_move == "Spock"
+          return 2
+        elsif @player2_move == "Paper" || @player2_move == "Lizard"
+          return 1
+        else
+          return nil
+        end
+      elsif @player1_move == "Spock"
+        if @player2_move == "Paper" || @player2_move == "Lizard"
+          return 2
+        elsif @player2_move == "Rock" || @player2_move == "Scissors"
+          return 1
+        else
+          return nil
+        end  
+      elsif @player1_move == "Lizard"
+        if @player2_move == "Rock" || @player2_move == "Scissors"
+          return 2
+        elsif @player2_move == "Spock" || @player2_move == "Paper"
+          return 1
+        else
+          return nil
+        end
+      end    
+  end
+
+  def track_player_wins(num)
+
+    if num == 1
+      @player1_wins += 1
+    elsif num == 2
+      @player2_wins += 1
+    end
+  end
+
+  def game_outcomes(num)
+    if num == 1
+      puts "\n#{@player1_move} beats #{@player2_move}! Player 1 Wins!"
+      puts "--------------------------------------------------------------------"
+    elsif num == 2
+      puts "\n#{@player2_move} beats #{@player1_move}! Player 2 Wins!"
+      puts "--------------------------------------------------------------------"
+    else
+      puts "\nIt's a Tie! Get Ready for Game #{(@which_game + 1)}!"
+      puts "--------------------------------------------------------------------"
+    end
+  end
+
+
+  def display_set_winner
+    if @player1_wins > @player2_wins
+        puts "\nCongratulations Player 1! You take the cake with a score of #{@player1_wins} to #{@player2_wins}!"
+      elsif @player2_wins > @player1_wins
+        puts "\nCongratulations Player 2! You take the cake with a score of #{@player2_wins} to #{@player1_wins}!"
+      else
+        puts "\nWhat a grueling battle with no clear winner! Care to settle the score?"
+    end
+  end
+
+  def rock_paper_scissors
+    get_total_games
+    
+    while @which_game <= @total_games
+      
+      if @player1_wins == @wins_needed || @player2_wins == @wins_needed
+          break
+      end
+
+      game_format_setup  
+      get_player1_move
+      get_player2_move
+      
+      winner = determine_game_winner
+      track_player_wins(winner)
+      game_outcomes(winner)
+
+      @which_game += 1
+    end
+
+    display_set_winner       
+  end
+
 end
--- tests/rock_paper_scissors_test.rb
+++ tests/rock_paper_scissors_test.rb
@@ -4,73 +4,121 @@ require 'rock-paper-scissors'
 class RockPaperScissorsTest < Minitest::Test
 
   def test_player1_win1
-    $player1_move = "Rock"
-    $player2_move = "Scissors"
+    test_game = Game.new
+    
+    test_game.set_player1_move("Rock")
+    test_game.set_player2_move("Scissors")
 
-    assert_equal(1, determine_game_winner)
+    assert_equal(1, test_game.determine_game_winner)
+
+  end
+  
+  def test_player1_win1a
+      test_game = Game.new
+      
+      test_game.set_player1_move("Rock")
+      test_game.set_player2_move("Lizard")
+
+      assert_equal(1, test_game.determine_game_winner)
+
+  end
+  
+  def test_player1_win2
+    test_game = Game.new
+
+    test_game.set_player1_move("Paper")
+    test_game.set_player2_move("Rock")
+
+    assert_equal(1, test_game.determine_game_winner)
 
   end
 
-  def test_player1_win2
-    $player1_move = "Paper"
-    $player2_move = "Rock"
+  def test_player1_win2a
+    test_game = Game.new
 
-    assert_equal(1, determine_game_winner)
+    test_game.set_player1_move("Paper")
+    test_game.set_player2_move("Spock")
+
+    assert_equal(1, test_game.determine_game_winner)
 
   end
 
   def test_player1_win3
-      $player2_move = "Scissors"
-      $player1_move = "Paper"
+    test_game = Game.new
 
-      assert_equal(2, determine_game_winner)
+    test_game.set_player2_move("Scissors")
+    test_game.set_player1_move("Paper")
+
+    assert_equal(2, test_game.determine_game_winner)
 
   end
 
-  def test_player2_win1
-    $player2_move = "Rock"
-    $player1_move = "Scissors"
+  def test_player1_win3a
+    test_game = Game.new
 
-    assert_equal(2, determine_game_winner)
+    test_game.set_player2_move("Scissors")
+    test_game.set_player1_move("Lizard")
+
+    assert_equal(2, test_game.determine_game_winner)
+
+  end
+  
+  def test_player2_win1
+    test_game = Game.new
+
+    test_game.set_player2_move("Rock")
+    test_game.set_player1_move("Scissors")
+
+    assert_equal(2, test_game.determine_game_winner)
 
   end
 
   def test_player2_win2
-    $player2_move = "Paper"
-    $player1_move = "Rock"
+    test_game = Game.new
 
-    assert_equal(2, determine_game_winner)
+    test_game.set_player2_move("Paper")
+    test_game.set_player1_move("Rock")
+
+    assert_equal(2, test_game.determine_game_winner)
 
 
   end
 
   def test_player2_win3
-    $player2_move = "Scissors"
-    $player1_move = "Paper"
+    test_game = Game.new
 
-    assert_equal(2, determine_game_winner)
+    test_game.set_player2_move("Scissors")
+    test_game.set_player1_move("Paper")
+
+    assert_equal(2, test_game.determine_game_winner)
 
   end
 
   def test_tie1
-    $player1_move = "Rock"
-    $player2_move = "Rock"
+    test_game = Game.new
 
-    assert_equal(nil, determine_game_winner)
+    test_game.set_player1_move("Rock")
+    test_game.set_player2_move("Rock")
+
+    assert_equal(nil, test_game.determine_game_winner)
   end
 
   def test_tie2
-    $player1_move = "Paper"
-    $player2_move = "Paper"
+    test_game = Game.new
 
-    assert_equal(nil, determine_game_winner)
+    test_game.set_player1_move("Paper")
+    test_game.set_player2_move("Paper")
+
+    assert_equal(nil, test_game.determine_game_winner)
   end
 
   def test_tie3
-    $player1_move = "Scissors"
-    $player2_move = "Scissors"
+    test_game = Game.new
 
-    assert_equal(nil, determine_game_winner)
+    test_game.set_player1_move("Scissors")
+    test_game.set_player2_move("Scissors")
+
+    assert_equal(nil, test_game.determine_game_winner)
   end
 
 end
